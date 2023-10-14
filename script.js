90b4383c6a810840ed6e485db2cbe2a08a90eff7
musaddique333 <mohammedmusaddiquemogal@gamil.com>
2023-10-14 14:52:21 +0100

    game created

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ const scissors = document.querySelector('#scissors');
 const options = ['rock', 'paper', 'scissors'];
 
 const score = document.querySelector('.score');
+const points = document.querySelector('.points');
+const message = document.querySelector('.message');
+let playerScore = 0;
+let computerScore = 0;
+
+const score_update = () => {
+    points.innerHTML = `${playerScore} - ${computerScore}`;
+};
+
+setTimeout(() => {message.innerHTML = "S. T. A. R. T"}, 9000);
+document.addEventListener('click', () => {message.innerHTML = ""});
+
 
 const game = () => {
     score.innerHTML = options[Math.floor(Math.random() * 3)];
@@ -12,29 +24,45 @@ const game = () => {
     if (chosen == 'rock') {
         if (score.innerHTML == 'paper') {
             score.innerHTML = 'You lose!';
+            computerScore++;
+            score_update();
         } else if (score.innerHTML == 'scissors') {
             score.innerHTML = 'You win!';
+            playerScore++;
+            score_update();
         } else {
             score.innerHTML = 'Draw!';
+            score_update();
         }
     }
     else if (chosen == 'paper') {
         if (score.innerHTML == 'scissors') {
             score.innerHTML = 'You lose!';
+            computerScore++;
+            score_update();
         } else if (score.innerHTML == 'rock') {
             score.innerHTML = 'You win!';
+            playerScore++;
+            score_update();
         } else {
             score.innerHTML = 'Draw!';
+            score_update();
         }
     }
     else if (chosen == 'scissors'){
         if (score.innerHTML == 'rock') {
             score.innerHTML = 'You lose!';
+            computerScore++;
+            score_update();
         } else if (score.innerHTML == 'paper') {
             score.innerHTML = 'You win!';
+            playerScore++;
+            score_update();
         } else {
             score.innerHTML = 'Draw!';
+            score_update();
         }
+        score_update();
     }
 };
 
